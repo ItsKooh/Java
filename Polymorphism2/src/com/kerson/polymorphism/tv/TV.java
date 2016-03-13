db3649a54e4e561113da776a91184bf18df30364
고동균 <itskooh@Kerson-iMac.local>
2016-03-13 23:54:51 +0900

    Polymorphism2 프로젝트 최초 커밋

---
+++ Polymorphism2/src/com/kerson/polymorphism/tv/TV.java
@@ -0,0 +1,10 @@
+package com.kerson.polymorphism.tv;
+
+public interface TV {
+	
+	public void turnOn(); 
+	public void turnOff();
+	public void changeChannel();
+	public void soundUp(); 
+	public void soundDown();
+}
